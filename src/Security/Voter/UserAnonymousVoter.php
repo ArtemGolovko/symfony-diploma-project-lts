18f99167f1ffd8cee2ca51d96f370f0859f796a9
Artem Golovko <artem.golovko2008@gmail.com>
2022-12-16 18:47:44 +0200

    Создал воутер  UserAnonymousVoter и воутер UserVerifiedVoter.

---
+++ src/Security/Voter/UserAnonymousVoter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserAnonymousVoter extends Voter
+{
+    public const IS_ANONYMOUS = 'IS_ANONYMOUS';
+
+    protected function supports($attribute, $subject): bool
+    {
+        return $attribute === self::IS_ANONYMOUS;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        return !$user instanceof UserInterface;
+    }
+}
