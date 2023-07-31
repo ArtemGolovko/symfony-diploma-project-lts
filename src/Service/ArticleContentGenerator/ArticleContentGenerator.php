cadc3c4b03067dac26b0390e0fe620a07425fe97
Artem Golovko <artem.golovko2008@gmail.com>
2023-07-31 11:39:41 +0300

    Добавил генерацию заголовка и абзацов.

--- src/Service/ArticleContentGenerator/ArticleContentGenerator.php
+++ src/Service/ArticleContentGenerator/ArticleContentGenerator.php
@@ -3,6 +3,7 @@
 namespace App\Service\ArticleContentGenerator;
 
 use App\Entity\ValueObject\ArticleGenerateOptions;
+use App\Entity\ValueObject\Range;
 use App\Entity\ValueObject\Subscription;
 use App\Twig\ArticleGeneratorExtension;
 use App\Twig\Wrapper\KeywordWrapper;
@@ -10,6 +11,8 @@ use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\SyntaxError;
 use Twig\Loader\ArrayLoader;
+use Twig\Source;
+use Twig\Token;
 
 class ArticleContentGenerator
 {
@@ -19,11 +22,30 @@ class ArticleContentGenerator
     private ThemeChain $themeProvider;
 
     /**
-     * @param ThemeChain $themeProvider
+     * @var ModuleProviderInterface
      */
-    public function __construct(ThemeChain $themeProvider)
+    private ModuleProviderInterface $moduleProvider;
+
+    /**
+     * @var Environment
+     */
+    private Environment $twig;
+
+    /**
+     * @param ThemeChain              $themeProvider
+     * @param ModuleProviderInterface $moduleProvider
+     */
+    public function __construct(ThemeChain $themeProvider, ModuleProviderInterface $moduleProvider)
     {
         $this->themeProvider = $themeProvider;
+        $this->moduleProvider = $moduleProvider;
+
+        $loader = new ArrayLoader([]);
+
+        $twig = new Environment($loader);
+        $twig->addExtension(new ArticleGeneratorExtension());
+
+        $this->twig = $twig;
     }
 
     /**
@@ -39,18 +61,20 @@ class ArticleContentGenerator
     public function generate(ArticleGenerateOptions $options, Subscription $subscription): string
     {
         $theme = $this->getTheme($options);
-        $paragraph = $this->getRandomParagraph($theme);
 
-        $loader = new ArrayLoader([]);
+        $keyword = new KeywordWrapper($options->getKeywords());
 
-        $twig = new Environment($loader);
-        $twig->addExtension(new ArticleGeneratorExtension());
+        $modules = $this->getModules($options->getSize());
+        $title = $this->generateTitle($theme, $options->getTitle(), $keyword);
 
-        $template = $twig->createTemplate($paragraph);
+        $paragraphsAmount = $this->getParagraphsAmount($modules);
+        $paragraphs = [];
 
-        return $template->render([
-            'keyword' => new KeywordWrapper($options->getKeywords()),
-        ]);
+        for ($i = 0; $i < $paragraphsAmount; $i++) {
+            $paragraphs[] = $this->generateParagraph($theme, $keyword);
+        }
+
+        dd($title, $paragraphs);
     }
 
     /**
@@ -86,14 +110,93 @@ class ArticleContentGenerator
     }
 
     /**
-     * @param Theme $theme
+     * @param Range $range
+     *
+     * @return ModuleInterface[]
+     */
+    private function getModules(Range $range): array
+    {
+        $modules = $this->moduleProvider->getModules();
+        $indexes = array_rand($modules, $range->generate());
+
+        $selectedModules = [];
+
+        foreach ($indexes as $index) {
+            $selectedModules[] = $modules[$index];
+        }
+
+        return $selectedModules;
+    }
+
+    /**
+     * @param ModuleInterface[] $modules
+     *
+     * @return int
+     * @throws SyntaxError
+     */
+    private function getParagraphsAmount(array $modules): int
+    {
+        $paragraphsAmount = 0;
+
+        foreach ($modules as $module) {
+            $tokens = $this->twig->tokenize(new Source($module->getTemplate(), 'module'));
+
+            for (; !$tokens->isEOF(); $tokens->next()) {
+                $token = $tokens->getCurrent();
+
+                if ($token->test(Token::NAME_TYPE, 'paragraph')) {
+                    $paragraphsAmount += 1;
+                }
+
+                if ($token->test(Token::NAME_TYPE, 'paragraphs')) {
+                    $paragraphsAmount += mt_rand(1, 3);
+                }
+            }
+        }
+
+        return $paragraphsAmount;
+    }
+
+    /**
+     * @param Theme          $theme
+     * @param KeywordWrapper $keyword
      *
      * @return string
+     * @throws LoaderError
+     * @throws SyntaxError
      */
-    private function getRandomParagraph(Theme $theme): string
+    private function generateParagraph(Theme $theme, KeywordWrapper $keyword): string
     {
         $index = array_rand($theme->paragraphs);
+        $paragraph = $theme->paragraphs[$index];
 
-        return $theme->paragraphs[$index];
+        $template = $this->twig->createTemplate($paragraph);
+
+        return $template->render([
+            'keyword' => $keyword,
+        ]);
+    }
+
+    /**
+     * @param Theme                  $theme
+     * @param ArticleGenerateOptions $options
+     *
+     * @return string
+     * @throws LoaderError
+     * @throws SyntaxError
+     */
+    private function generateTitle(Theme $theme, ?string $title, KeywordWrapper $keyword): string
+    {
+        if (null === $title) {
+            $index = array_rand($theme->titles);
+
+            $title = $theme->titles[$index];
+        }
+
+        $template = $this->twig->createTemplate($title);
+
+        return $template->render([
+            'keyword' => $keyword,
+        ]);
     }
 }
