007d8cfa1c70b32e7c6d37c0931f5d9241036ffa
Artem Golovko <artem.golovko2008@gmail.com>
2023-07-31 14:11:37 +0300

    Изменил генерацию абзацев, и добавил вставку продвигаємих слов.

--- src/Service/ArticleContentGenerator/ArticleContentGenerator.php
+++ src/Service/ArticleContentGenerator/ArticleContentGenerator.php
@@ -13,6 +13,7 @@ use Twig\Error\SyntaxError;
 use Twig\Loader\ArrayLoader;
 use Twig\Source;
 use Twig\Token;
+use Twig\TokenStream;
 
 class ArticleContentGenerator
 {
@@ -67,12 +68,9 @@ class ArticleContentGenerator
         $modules = $this->getModules($options->getSize());
         $title = $this->generateTitle($theme, $options->getTitle(), $keyword);
 
-        $paragraphsAmount = $this->getParagraphsAmount($modules);
-        $paragraphs = [];
+        $paragraphsDistribution = $this->getParagraphsDistribution($modules);
 
-        for ($i = 0; $i < $paragraphsAmount; $i++) {
-            $paragraphs[] = $this->generateParagraph($theme, $keyword);
-        }
+        $paragraphs = $this->generateParagraphs($paragraphsDistribution, $theme, $options);
 
         dd($title, $paragraphs);
     }
@@ -131,12 +129,12 @@ class ArticleContentGenerator
     /**
      * @param ModuleInterface[] $modules
      *
-     * @return int
+     * @return int[]
      * @throws SyntaxError
      */
-    private function getParagraphsAmount(array $modules): int
+    private function getParagraphsDistribution(array $modules): array
     {
-        $paragraphsAmount = 0;
+        $paragraphsDistribution = [];
 
         foreach ($modules as $module) {
             $tokens = $this->twig->tokenize(new Source($module->getTemplate(), 'module'));
@@ -145,41 +143,203 @@ class ArticleContentGenerator
                 $token = $tokens->getCurrent();
 
                 if ($token->test(Token::NAME_TYPE, 'paragraph')) {
-                    $paragraphsAmount += 1;
+                    $paragraphsDistribution[] = 1;
                 }
 
                 if ($token->test(Token::NAME_TYPE, 'paragraphs')) {
-                    $paragraphsAmount += mt_rand(1, 3);
+                    $paragraphsDistribution[] = mt_rand(1, 3);
                 }
             }
         }
 
-        return $paragraphsAmount;
+        return $paragraphsDistribution;
+    }
+
+    /**
+     * @param int[]                  $distribution
+     * @param Theme                  $theme
+     * @param ArticleGenerateOptions $options
+     *
+     * @return string[][]
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
+    private function generateParagraphs(array $distribution, Theme $theme, ArticleGenerateOptions $options): array
+    {
+        $paragraphsAmount = array_sum($distribution);
+        /** @var TokenStream[] $paragraphs */
+        $paragraphs = [];
+
+        for ($i = 0; $i < $paragraphsAmount; $i++) {
+            $index = array_rand($theme->paragraphs);
+            $paragraph = $theme->paragraphs[$index];
+            $paragraphs[] = $this->twig->tokenize(new Source($paragraph, 'paragraph'));
+        }
+
+        $promotedWords = $options->getPromotedWords();
+        $paragraphContext = [];
+
+        foreach ($promotedWords as $key => $promotedWord) {
+            $paragraphContext['promoted_word_' . $key] = $promotedWord->getWord();
+
+            for ($i = 0; $i < $promotedWord->getRepetitions(); $i++) {
+                $index = array_rand($paragraphs);
+
+                $paragraphs[$index] = $this->injectPromotedWord(
+                    $paragraphs[$index],
+                    'promoted_word_' . $key
+                );
+            }
+        }
+        $renderedParagraphs = [];
+        $paragraphContext['keyword'] = new KeywordWrapper($options->getKeywords());
+
+        foreach ($paragraphs as $paragraph) {
+            $paragraphTemplate = $this->recompileTokens($paragraph);
+            $template = $this->twig->createTemplate($paragraphTemplate);
+
+            $renderedParagraphs[] = $template->render($paragraphContext);
+        }
+
+        $renderedParagraphs = array_reverse($renderedParagraphs);
+        $shapedParagraphs = [];
+
+        foreach ($distribution as $key => $amount) {
+            $shapedParagraphs[] = [];
+            for ($i = 0; $i < $amount; $i++) {
+                $shapedParagraphs[$key][] = array_pop($renderedParagraphs);
+            }
+        }
+
+        return $shapedParagraphs;
+    }
+
+    /**
+     * @param TokenStream $paragraph
+     * @param string      $wordPlaceholder
+     *
+     * @return TokenStream
+     * @throws SyntaxError
+     */
+    private function injectPromotedWord(TokenStream $paragraph, string $wordPlaceholder): TokenStream
+    {
+        /** @var Token[] $tokens */
+        $tokens = [];
+        $textTokenIndexes = [];
+
+        for (; !$paragraph->isEOF(); $paragraph->next()) {
+            $tokens[] = $paragraph->getCurrent();
+        }
+
+        $tokens[] = $paragraph->getCurrent();
+
+        foreach ($tokens as $index => $token) {
+            if ($token->test(Token::TEXT_TYPE)) {
+                $textTokenIndexes[] = $index;
+            }
+        }
+
+        $index = array_rand($textTokenIndexes);
+        $textTokenIndex = $textTokenIndexes[$index];
+
+        $textToken = $tokens[$textTokenIndex];
+
+        $words = explode(' ', $textToken->getValue());
+
+        $wordIndex = array_rand($words);
+        $wordsBefore = [];
+        $wordsAfter = [];
+
+        foreach ($words as $index => $word) {
+            if ($index <= $wordIndex) {
+                $wordsBefore[] = $word;
+                continue;
+            }
+
+            $wordsAfter[] = $word;
+        }
+
+        $textBefore = implode(' ', $wordsBefore) . ' ';
+
+        $textAfter = ' ' . implode(' ', $wordsAfter);
+
+        $tokensBefore = array_slice($tokens, 0, $textTokenIndex);
+        $tokensAfter = array_slice($tokens, $textTokenIndex + 1);
+
+        $lineno = $textToken->getLine();
+
+        $insertTokens = [
+            new Token(Token::TEXT_TYPE, $textBefore, $lineno),
+            new Token(Token::VAR_START_TYPE, '', $lineno),
+            new Token(Token::NAME_TYPE, $wordPlaceholder, $lineno),
+            new Token(Token::VAR_END_TYPE, '', $lineno),
+            new Token(Token::TEXT_TYPE, $textAfter, $lineno),
+        ];
+
+        $insertTokens = array_filter($insertTokens, function (Token $token) {
+            return !$token->test(Token::TEXT_TYPE, '');
+        });
+
+        $newTokens = array_merge($tokensBefore, $insertTokens, $tokensAfter);
+
+        return new TokenStream($newTokens);
+    }
+
+    /**
+     * @param TokenStream $tokens
+     *
+     * @return string
+     * @throws SyntaxError
+     */
+    private function recompileTokens(TokenStream $tokens): string
+    {
+        $token_array = [];
+
+        for (; !$tokens->isEOF(); $tokens->next()) {
+            $token_array[] = $tokens->getCurrent();
+        }
+
+        return implode(
+            '',
+            array_map(function (Token $token) {
+                switch ($token->getType()) {
+                    case Token::TEXT_TYPE:
+                    case Token::NAME_TYPE:
+                    case Token::NUMBER_TYPE:
+                        return $token->getValue();
+                    case Token::BLOCK_START_TYPE:
+                        return '{% ';
+                    case Token::VAR_START_TYPE:
+                        return '{{ ';
+                    case Token::BLOCK_END_TYPE:
+                        return ' %}';
+                    case Token::VAR_END_TYPE:
+                        return ' }}';
+                    case Token::STRING_TYPE:
+                        return "'" . $token->getValue() . "'";
+                    case Token::OPERATOR_TYPE:
+                        return ' ' . $token->getValue() . ' ';
+                    case Token::PUNCTUATION_TYPE:
+                        return $token->getValue() . ' ';
+                    case Token::INTERPOLATION_START_TYPE:
+                        return '#{';
+                    case Token::INTERPOLATION_END_TYPE:
+                        return '}';
+                    case Token::ARROW_TYPE:
+                        return ' => ';
+                    case Token::EOF_TYPE:
+                        return '';
+                }
+
+                return '';
+            }, $token_array)
+        );
     }
 
     /**
      * @param Theme          $theme
+     * @param string|null    $title
      * @param KeywordWrapper $keyword
-     *
-     * @return string
-     * @throws LoaderError
-     * @throws SyntaxError
-     */
-    private function generateParagraph(Theme $theme, KeywordWrapper $keyword): string
-    {
-        $index = array_rand($theme->paragraphs);
-        $paragraph = $theme->paragraphs[$index];
-
-        $template = $this->twig->createTemplate($paragraph);
-
-        return $template->render([
-            'keyword' => $keyword,
-        ]);
-    }
-
-    /**
-     * @param Theme                  $theme
-     * @param ArticleGenerateOptions $options
      *
      * @return string
      * @throws LoaderError
